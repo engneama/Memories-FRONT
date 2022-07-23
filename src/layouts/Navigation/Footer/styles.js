5932ee058a615487de2bdf45bd6663014d5ce903
Tno-MSA <tno.rappelz@gmail.com>
2022-07-24 01:25:22 +0300

    changed the padding values

--- src/layouts/Navigation/Footer/styles.js
+++ src/layouts/Navigation/Footer/styles.js
@@ -2,7 +2,6 @@ import { createStyles } from "@mantine/core";
 
 export const useStyles = createStyles((theme) => ({
   footer: {
-    marginTop: 40,
     borderTop: `1px solid ${
       theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
     }`,
@@ -12,8 +11,8 @@ export const useStyles = createStyles((theme) => ({
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
-    paddingTop: theme.spacing.xl,
-    paddingBottom: theme.spacing.xl,
+    paddingTop: theme.spacing.md,
+    paddingBottom: theme.spacing.md,
 
     [theme.fn.smallerThan("xs")]: {
       flexDirection: "column",
