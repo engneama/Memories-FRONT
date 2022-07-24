d08e12684b896b8798f807babf9ca653e64761be
Tno-MSA <tno.rappelz@gmail.com>
2022-07-24 18:38:05 +0300

    add the notifications call.

--- src/store/auth/auth.slice.js
+++ src/store/auth/auth.slice.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import * as thunk from "./auth.thunk";
+import notifications from "components/common/Notifications";
 
 const initialState = {
   user: null,
@@ -11,10 +12,47 @@ const authSlice = createSlice({
   extraReducers: (builder) => {
     builder
       //REGISTER
+      .addCase(thunk.register.pending, (state, action) => {
+        notifications.Pending(
+          "auth/register",
+          "Hold on",
+          "Please wait while we handle your registration..."
+        );
+      })
+      .addCase(thunk.register.rejected, (state, action) => {
+        console.log(action);
+        notifications.Failure(
+          "auth/register",
+          "Uh Oh!",
+          action.payload.message || "Something went wrong. Please try again."
+        );
+      })
+      .addCase(thunk.register.fulfilled, (state, action) => {
+        console.log(action);
+        notifications.Success("auth/register", "Done!", action.payload.message);
+      })
       //REGISTER
       //LOGIN
+      .addCase(thunk.login.pending, (state, action) => {
+        notifications.Pending(
+          "auth/login",
+          "Hold on",
+          "Please wait while we handle your loggin..."
+        );
+      })
+      .addCase(thunk.login.rejected, (state, action) => {
+        console.log(action);
+        notifications.Failure(
+          "auth/login",
+          "Uh Oh!",
+          action.payload.response.data.message ||
+            "Something went wrong. Please try again."
+        );
+      })
       .addCase(thunk.login.fulfilled, (state, action) => {
         state.user = action.payload;
+        console.log(action);
+        notifications.Success("auth/login", "Done!", "Welcome home ❤️");
       })
       //LOGIN
       //VERIFY TOKEN
