1366006e2fdd822c76adf9794fd6e9c59dc03959
Tno-MSA <tno.rappelz@gmail.com>
2022-07-25 19:09:53 +0300

    changed the path

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,70 @@
+//Hooks
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useStyles } from "./styles";
+//Actions
+import { logout } from "store/auth/auth.thunk";
+//UI Components
+import { Avatar, UnstyledButton, Text } from "@mantine/core";
+import { Menu, Divider, Group } from "@mantine/core";
+//Icons
+import { FaHeart } from "react-icons/fa";
+import { TbLogout, TbMessage, TbChevronDown } from "react-icons/tb";
+
+const UserMenu = ({ user }) => {
+  const dispatch = useDispatch();
+  const { classes, theme, cx } = useStyles();
+  const [userMenuOpened, setUserMenuOpened] = useState(false);
+
+  const logoutHandler = async () => {
+    await dispatch(logout());
+  };
+
+  return (
+    <Menu
+      size={260}
+      placement="end"
+      transition="pop-top-right"
+      onClose={() => setUserMenuOpened(false)}
+      onOpen={() => setUserMenuOpened(true)}
+      control={
+        <UnstyledButton
+          className={cx(classes.user, {
+            [classes.userActive]: userMenuOpened,
+          })}
+        >
+          <Group spacing={7}>
+            {/* User's Avatar */}
+            <Avatar src={user.avatar} alt={user.avatar} radius="xl" size={20} />
+            {/* User name */}
+            <Text weight={500} size="sm" sx={{ lineHeight: 1 }} mr={3}>
+              {user.username}
+            </Text>
+            <TbChevronDown size={12} />
+          </Group>
+        </UnstyledButton>
+      }
+    >
+      <Menu.Label>Your Legacy</Menu.Label>
+      <Menu.Item icon={<FaHeart size={14} color={theme.colors.red[6]} />}>
+        Liked posts
+      </Menu.Item>
+      <Menu.Item icon={<TbMessage size={16} color={theme.colors.blue[6]} />}>
+        Your comments
+      </Menu.Item>
+
+      <Divider />
+
+      <Menu.Label>Logout</Menu.Label>
+      <Menu.Item
+        color="red"
+        icon={<TbLogout size={16} />}
+        onClick={logoutHandler}
+      >
+        Logout
+      </Menu.Item>
+    </Menu>
+  );
+};
+
+export default UserMenu;
