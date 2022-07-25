6fca8740e536fe97ae4cd49b986d11bb0b701cb7
Tno-MSA <tno.rappelz@gmail.com>
2022-07-25 19:08:44 +0300

    changed the text logo to SVGs

--- src/components/common/Logo/ImageLogo.jsx
+++ src/components/common/Logo/ImageLogo.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import { Image, Text, Box } from "@mantine/core";
-import { Logo } from "configs";
+
+const URL =
+  "https://res.cloudinary.com/tno/image/upload/w_64,dpr_auto/v1656717410/memoriesLogo_jydozc.webp";
 
 const ImageLogo = () => {
   return (
     <Box component={Link} to="/">
       <Image
-        src={Logo.img}
-        alt={Logo.img}
+        src={URL}
+        alt="Memories image logo"
         ml="lg"
         mr="xs"
         width="2.25em"
