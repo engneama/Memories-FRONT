cdca64e74163e88fa33454f7b1a7c32b32ed69d1
Tno-MSA <tno.rappelz@gmail.com>
2022-07-25 23:58:36 +0300

    the verifyCode endpoint

--- src/services/axios/auth.js
+++ src/services/axios/auth.js
@@ -8,9 +8,12 @@ const logout = async () => API.get("/auth/logout");
 
 const verifyToken = async () => API.get("/auth/verifyToken");
 
+const verifyCode = async (data) => API.get(`/auth/verifyCode?code=${data}`);
+
 export default {
   register,
   login,
   logout,
+  verifyCode,
   verifyToken,
 };
