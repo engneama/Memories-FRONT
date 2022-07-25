8a522ab0ee75d7533c04246727db54ec517e7345
Tno-MSA <tno.rappelz@gmail.com>
2022-07-25 19:10:05 +0300

    *poof*

--- src/configs/Logo.js
+++
@@ -1,6 +0,0 @@
-export default {
-  text: "https://res.cloudinary.com/tno/image/upload/w_256,dpr_auto/v1656717410/memoriesText_ktyidi.webp",
-  textWhite:
-    "https://res.cloudinary.com/tno/image/upload/w_256,dpr_auto/v1658656405/WHITE_LOGO_zjckdp.webp",
-  img: "https://res.cloudinary.com/tno/image/upload/w_64,dpr_auto/v1656717410/memoriesLogo_jydozc.png",
-};
