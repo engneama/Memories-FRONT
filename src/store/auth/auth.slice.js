65378f70c2f38f6283fe64a0e8d1fa31c5e0c8f6
Tno-MSA <tno.rappelz@gmail.com>
2022-07-25 19:15:36 +0300

    removed the notifications lines

--- src/store/auth/auth.slice.js
+++ src/store/auth/auth.slice.js
@@ -12,48 +12,10 @@ const authSlice = createSlice({
   extraReducers: (builder) => {
     builder
       //REGISTER
-      .addCase(thunk.register.pending, (state, action) => {
-        notifications.Pending(
-          "auth/register",
-          "Hold on",
-          "Please wait while we handle your registration..."
-        );
-      })
-      .addCase(thunk.register.rejected, (state, action) => {
-        console.log(action);
-        notifications.Failure(
-          "auth/register",
-          "Uh Oh!",
-          action.payload.response.data.message ||
-            "Something went wrong. Please try again."
-        );
-      })
-      .addCase(thunk.register.fulfilled, (state, action) => {
-        console.log(action);
-        notifications.Success("auth/register", "Done!", action.payload.message);
-      })
       //REGISTER
       //LOGIN
-      .addCase(thunk.login.pending, (state, action) => {
-        notifications.Pending(
-          "auth/login",
-          "Hold on",
-          "Please wait while we handle your loggin..."
-        );
-      })
-      .addCase(thunk.login.rejected, (state, action) => {
-        console.log(action);
-        notifications.Failure(
-          "auth/login",
-          "Uh Oh!",
-          action.payload.response.data.message ||
-            "Something went wrong. Please try again."
-        );
-      })
       .addCase(thunk.login.fulfilled, (state, action) => {
         state.user = action.payload;
-        console.log(action);
-        notifications.Success("auth/login", "Done!", "Welcome home ❤️");
       })
       //LOGIN
       //VERIFY TOKEN
