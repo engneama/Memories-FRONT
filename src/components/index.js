69e9475cda10e66bdfd5a7da6331b17b75840a86
Tno-MSA <tno.rappelz@gmail.com>
2022-07-26 21:17:04 +0300

    index ?

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import common from "./common";
+import Auth from "./Auth";
+import ControlledPasswordField from "./ControlledPasswordField/ControlledPasswordField";
+import SwitchTheme from "./SwitchThemesButton/SwitchThemeButton";
+import UserMenu from "./UserMenu/UserMenu";
+
+export { common, Auth, ControlledPasswordField, SwitchTheme, UserMenu };
