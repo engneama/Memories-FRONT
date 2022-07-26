c7bea34be96d0dcb10798601fc55a37a3379c76b
Tno-MSA <tno.rappelz@gmail.com>
2022-07-26 21:14:59 +0300

    index for common

---
+++ src/components/common/index.js
@@ -0,0 +1,5 @@
+import LoadingOverlay from "./Loader/Overlay";
+import ControlledField from "./ControlledInputField/ControlledField";
+import Alert from "./Alert/ResponseAlert";
+
+export { LoadingOverlay, ControlledField, Alert };
