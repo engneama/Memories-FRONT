556e7d06729b4759f79a3f42ec296ab71627e577
Tno-MSA <tno.rappelz@gmail.com>
2022-07-27 14:58:29 +0300

    created index

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Auth from "./auth";
+import User from "./user";
+import Missing from "./Missing/Missing";
+import Home from "./Home";
+
+export { Auth, User, Missing, Home };
---
+++ src/pages/user/index.js
@@ -0,0 +1,11 @@
+import Comments from "./Comments/Comments";
+import Likes from "./Likes/Likes";
+import CreateMemory from "./CreateMemory/CreateMemory";
+import CreateComment from "./CreateComment/CreateComment";
+
+export default {
+  Likes,
+  Comments,
+  CreateMemory,
+  CreateComment,
+};
