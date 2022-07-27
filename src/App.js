1d893e3581c8b0fb8fc762a4dae815e9ceeeb673
Tno-MSA <tno.rappelz@gmail.com>
2022-07-27 14:58:00 +0300

    re-order imports + import from index

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Main from "layouts/Main/Main";
 //Public Components
-import Home from "pages/Home";
-import Login from "pages/auth/Login/Login";
-import Register from "pages/auth/Register/Register";
+import { Home, Missing, Auth } from "pages";
 //Protected Components
-import RequireAuth from "components/Auth/RequireAuth";
-import CreateMemory from "pages/CreateMemory";
-import Activation from "pages/auth/Activation/Activation";
+import { User } from "pages";
 
+//Controll Access
+import RequireAuth from "components/Auth/RequireAuth";
 import { PersistLogin, NotRequireAuth } from "components/Auth";
-import Missing from "pages/Missing/Missing";
 
 const App = () => {
   return (
@@ -21,14 +18,16 @@ const App = () => {
           <Route index element={<Home />} />
           {/* Prevent LoggenIn users from accessing */}
           <Route element={<NotRequireAuth />}>
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="activation" element={<Activation />} />
+            <Route path="login" element={<Auth.Login />} />
+            <Route path="register" element={<Auth.Register />} />
+            <Route path="activation" element={<Auth.Activation />} />
           </Route>
 
           {/* protected routes */}
           <Route element={<RequireAuth />}>
-            <Route path="create" element={<CreateMemory />} />
+            <Route path="createMemory" element={<User.CreateMemory />} />
+            <Route path="likes" element={<User.Likes />} />
+            <Route path="comments" element={<User.Comments />} />
           </Route>
 
           {/* catch all */}
