3a98d7c11d039891090c30f32f5c3c905e8ecd5b
Tno-MSA <tno.rappelz@gmail.com>
2022-07-27 14:58:57 +0300

    removed common and auth

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
-import common from "./common";
-import Auth from "./Auth";
 import ControlledPasswordField from "./ControlledPasswordField/ControlledPasswordField";
 import SwitchTheme from "./SwitchThemesButton/SwitchThemeButton";
 import UserMenu from "./UserMenu/UserMenu";
 
-export { common, Auth, ControlledPasswordField, SwitchTheme, UserMenu };
+export { ControlledPasswordField, SwitchTheme, UserMenu };
