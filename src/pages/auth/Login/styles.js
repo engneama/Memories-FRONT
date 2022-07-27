c84bc01b8777204c03f0b8ee3f75ca21fe58effd
Tno-MSA <tno.rappelz@gmail.com>
2022-07-27 14:56:16 +0300

    added marginBottom

--- src/pages/auth/Login/styles.js
+++ src/pages/auth/Login/styles.js
@@ -3,6 +3,7 @@ import { createStyles } from "@mantine/core";
 export const useStyles = createStyles((theme) => ({
   section: {
     marginTop: theme.spacing.lg,
+    marginBottom: theme.spacing.lg,
   },
 
   title: {
