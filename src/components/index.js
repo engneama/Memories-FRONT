62b938f4e01ade645fb990ab6a31da7245a726d2
Tno-MSA <tno.rappelz@gmail.com>
2022-07-30 00:04:48 +0300

    added FloatingButton

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import SwitchTheme from "./SwitchThemesButton/SwitchThemeButton";
 import UserMenu from "./UserMenu/UserMenu";
 import RichEditor from "./RichTextEditor";
+import FloatingButton from "./FloatingButton/FloatingButton";
 
-export { SwitchTheme, UserMenu, RichEditor };
+export { SwitchTheme, UserMenu, RichEditor, FloatingButton };
