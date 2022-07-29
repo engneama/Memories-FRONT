2633f9aae25f22eb7a339d37e523dbc99e53fb69
Tno-MSA <tno.rappelz@gmail.com>
2022-07-29 23:24:42 +0300

    added with to paper to have the same width as reg

--- src/pages/auth/Login/styles.js
+++ src/pages/auth/Login/styles.js
@@ -13,9 +13,10 @@ export const useStyles = createStyles((theme) => ({
   },
 
   paper: {
-    borderRadius: theme.radius.md,
+    width: "23em",
     padding: 30,
     marginTop: 30,
+    borderRadius: theme.radius.md,
     boxShadow: theme.shadows.md,
   },
 }));
