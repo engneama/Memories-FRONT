ba728f606888be1d13f8dee44967f558cf7f3d99
Tno-MSA <tno.rappelz@gmail.com>
2022-07-29 20:45:42 +0300

    added title, paper classes

--- src/pages/user/CreateMemory/styes.js
+++ src/pages/user/CreateMemory/styes.js
@@ -2,7 +2,20 @@ import { createStyles } from "@mantine/core";
 
 export const useStyles = createStyles((theme) => ({
   section: {
+    width: 600,
     marginTop: theme.spacing.lg,
     marginBottom: theme.spacing.lg,
+  },
+
+  title: {
+    fontWeight: 900,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+  },
+
+  paper: {
+    borderRadius: theme.radius.md,
+    padding: 30,
+    marginTop: 30,
+    boxShadow: theme.shadows.md,
   },
 }));
