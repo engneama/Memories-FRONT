8667d353790e24d96e4f1f8d053f8d9476240c4a
Tno-MSA <tno.rappelz@gmail.com>
2022-07-30 00:05:35 +0300

    added FloatingButton and re-arranged imports

--- src/layouts/Main/Main.jsx
+++ src/layouts/Main/Main.jsx
@@ -1,11 +1,16 @@
+//Components
+import { Outlet } from "react-router-dom";
+//UI Components
 import Header from "layouts/Navigation/Header/Header";
 import Footer from "layouts/Navigation/Footer/Footer";
-import { Outlet } from "react-router-dom";
+import { FloatingButton } from "components";
+//Styles
 import "./Main.css";
 
 const Main = () => {
   return (
     <div className="parent">
+      <FloatingButton />
       <Header />
       <div className="children">
         <Outlet />
