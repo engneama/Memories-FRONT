4c35fe178f7664f93763e1dd6b8b69b758e9778a
Tno-MSA <tno.rappelz@gmail.com>
2022-08-12 17:43:03 +0300

    changed "image" with "cover"

--- src/rules/newMemory.js
+++ src/rules/newMemory.js
@@ -3,7 +3,7 @@ import * as yup from "yup";
 const schema = yup.object().shape({
   title: yup.string().trim().lowercase().required(),
   tags: yup.array().min(1, "You must have at least 1 tag").required(),
-  image: yup
+  cover: yup
     .string()
     .required("You must upload 1 image as a cover for your memeory"),
   body: yup
