05b03cb40abc7cf1cee685534e6165372a6fbd77
Tno-MSA <tno.rappelz@gmail.com>
2022-08-12 17:21:33 +0300

    added "Common" and "MemoryDetails"

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,14 @@ import SwitchTheme from "./SwitchThemesButton/SwitchThemeButton";
 import UserMenu from "./UserMenu/UserMenu";
 import RichEditor from "./RichTextEditor";
 import FloatingButton from "./FloatingButton/FloatingButton";
+import MemoryDetails from "./MemoryDetails";
+import Common from "./common";
 
-export { SwitchTheme, UserMenu, RichEditor, FloatingButton };
+export {
+  SwitchTheme,
+  UserMenu,
+  RichEditor,
+  FloatingButton,
+  MemoryDetails,
+  Common,
+};
