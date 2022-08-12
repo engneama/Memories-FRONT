26129fca0272283defb043a61cbb205af9316a09
Tno-MSA <tno.rappelz@gmail.com>
2022-08-12 17:39:34 +0300

    replaced fakeDatam added isLoading state

--- src/components/common/ControlledFields/tags.jsx
+++ src/components/common/ControlledFields/tags.jsx
@@ -1,18 +1,39 @@
+//Hooks
+import { useState, useEffect } from "react";
+//Actions
+import { memory } from "services";
 //UI Components
 import { MultiSelect } from "@mantine/core";
 import { useFormContext, Controller } from "react-hook-form";
 //Icons
 import { TbTags } from "react-icons/tb";
 
-//TODO: Remove this later
-const FAKE = ["tno", "msa", "mohammed", "sobhy", "omar", "hoda"];
-
 const Tags = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tags, setTags] = useState([]);
+
   const {
     trigger,
     control,
     formState: { errors },
   } = useFormContext();
+
+  const handleGetTags = async () => {
+    setIsLoading(true);
+
+    try {
+      const { data } = await memory.getTags();
+      setTags(data.data.tags);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    handleGetTags();
+  }, []);
 
   return (
     <Controller
@@ -23,13 +44,14 @@ const Tags = () => {
         <MultiSelect
           {...field}
           required
+          disabled={isLoading}
           name="tags"
           label="Tags"
           placeholder="Select or enter 3 tags"
           description="Select at least one tag"
           icon={<TbTags size={18} />}
           searchable //the ability to search for a certain tag
-          data={FAKE} //take an array of data to auto complete
+          data={tags} //take an array of data to auto complete
           limit={5} //number of suggestions at the same time
           maxSelectedValues={3} //max number of tags
           maxDropdownHeight={160}
@@ -49,22 +71,3 @@ const Tags = () => {
 };
 
 export default Tags;
-
-//TODO: delete later
-//  <MultiSelect
-// required
-// name="tags"
-// label="Tags"
-// placeholder="Select or enter 3 tags"
-// icon={<FaTags />}
-// searchable //the ability to search for a certain tag
-// data={FAKE} //take an array of data to auto complete
-// limit={5} //number of suggestions at the same time
-// maxSelectedValues={3} //max number of tags
-// maxDropdownHeight={160}
-// clearable //add a X button on the right to clear the field.
-// clearButtonLabel="Clear selection"
-// creatable //the ability to add new tags
-// getCreateLabel={(query) => `+ Create ${query}`} //text shown when adding new tag
-// onChange={(e) => onChangeHandler(e)}
-// />
