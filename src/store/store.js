698c0a6c6672e5a74fd74f41ece32a9f8813a390
Tno-MSA <tno.rappelz@gmail.com>
2022-08-12 17:43:46 +0300

    added "memories" and tweaked  devTools condition

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/auth.slice";
+import memoriesReducer from "./memories/memories.slice";
 
 const ENV = process.env.NODE_ENV === "development";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    memories: memoriesReducer,
   },
-  devTools: ENV ? true : false,
+  devTools: ENV,
 });
 
 export default store;
