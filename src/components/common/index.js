cfffc32498a54b2a2c19ddfd4c3f0b280df8c8ee
Tno-MSA <tno.rappelz@gmail.com>
2022-08-12 17:34:36 +0300

    added "Cards" and "UncontrolledFields"

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,6 +1,15 @@
 import LoadingOverlay from "./Loader/Overlay";
 import ControlledFields from "./ControlledFields";
+import UncontrolledFields from "./UncontrolledFields";
 import Alerts from "./Alert";
 import Logo from "./Logo";
+import Cards from "./Cards";
 
-export { LoadingOverlay, ControlledFields, Alerts, Logo };
+export default {
+  LoadingOverlay,
+  ControlledFields,
+  UncontrolledFields,
+  Alerts,
+  Logo,
+  Cards,
+};
