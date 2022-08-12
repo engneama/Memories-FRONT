0367f39028c97a6a88e16e0b9abf0ad686f6e731
Tno-MSA <tno.rappelz@gmail.com>
2022-08-12 17:38:30 +0300

    axios memory created

---
+++ src/services/axios/memory.js
@@ -0,0 +1,32 @@
+import { API } from "./options";
+
+//GET
+const getAll = () => API.get("/memory/getAll");
+const getSingle = (data) => API.get(`/memory/getSingle/${data._id}`);
+const getTags = () => API.get("/memory/getTags");
+const search = ({ query, tags }) =>
+  API.get(`/memory/search?query=${query}&tags=${tags}`);
+const getRecommendations = (data) =>
+  API.get(`/memory/recommendations/${data._id}`);
+
+//POST
+const create = (data) => API.post("/memory/create", data);
+
+//PATCH
+const update = (data) => API.patch("/memory/update", data);
+const like = (data) => API.patch("/memory/like", data);
+
+//DELETE
+const _delete = (data) => API.delete("/memory/delete", data);
+
+export default {
+  create,
+  getAll,
+  getSingle,
+  getTags,
+  getRecommendations,
+  search,
+  update,
+  like,
+  _delete,
+};
