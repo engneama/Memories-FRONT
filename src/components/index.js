81986c14ea1f741b4a1b2d64393a7c76e06b7fd3
Tno-MSA <tno.rappelz@gmail.com>
2022-08-22 02:30:57 +0300

    moved "RichEditor" and "MemoryDetails" out

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,7 @@
 import SwitchTheme from "./SwitchThemesButton/SwitchThemeButton";
-import UserMenu from "./UserMenu/UserMenu";
-import RichEditor from "./RichTextEditor";
+import UserMenu from "./Header/UserMenu/UserMenu";
 import FloatingButton from "./FloatingButton/FloatingButton";
-import MemoryDetails from "./MemoryDetails";
 import Common from "./common";
+import Comments from "./Comments";
 
-export {
-  SwitchTheme,
-  UserMenu,
-  RichEditor,
-  FloatingButton,
-  MemoryDetails,
-  Common,
-};
+export { SwitchTheme, UserMenu, FloatingButton, Common, Comments };
