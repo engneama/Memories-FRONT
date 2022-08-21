4a65b1146cec78486b0f6b8e2fbde81deabad997
Tno-MSA <tno.rappelz@gmail.com>
2022-08-22 02:35:09 +0300

    added "Notifications"

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,11 +1,13 @@
 import LoadingOverlay from "./Loader/Overlay";
 import ControlledFields from "./ControlledFields";
 import UncontrolledFields from "./UncontrolledFields";
+import Notifications from "./Notifications";
 import Alerts from "./Alert";
 import Logo from "./Logo";
 import Cards from "./Cards";
 
 export default {
+  Notifications,
   LoadingOverlay,
   ControlledFields,
   UncontrolledFields,
