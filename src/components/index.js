b7f02ffdb33c929ba420f9eebd7952640941e3c0
Tno-MSA <tno.rappelz@gmail.com>
2022-08-22 23:46:31 +0300

    added "MainPage"

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import UserMenu from "./Header/UserMenu/UserMenu";
 import FloatingButton from "./FloatingButton/FloatingButton";
 import Common from "./common";
 import Comments from "./Comments";
+import MainPage from "./MainPage";
 
-export { SwitchTheme, UserMenu, FloatingButton, Common, Comments };
+export { SwitchTheme, UserMenu, FloatingButton, Common, Comments, MainPage };
