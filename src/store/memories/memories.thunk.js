d0287662e64b7403a9f2009b441ae2d863fa4777
Tno-MSA <tno.rappelz@gmail.com>
2022-08-22 04:08:04 +0300

    added 3 cases to getall, create, update, del, like

--- src/store/memories/memories.thunk.js
+++ src/store/memories/memories.thunk.js
@@ -37,3 +37,69 @@ export const create = createAsyncThunk(
     }
   }
 );
+
+export const update = createAsyncThunk(
+  "memories/update",
+  async (memoryData, thunkAPI) => {
+    try {
+      const { data } = await memory.update(memoryData);
+      const userData = await cookieExtractor(data.accessToken.data.accessToken);
+      thunkAPI.dispatch(addUser(userData));
+      return data;
+    } catch (error) {
+      if (
+        error.response.data?.accessToken ||
+        error.response.data?.refreshToken
+      ) {
+        thunkAPI.dispatch(removeUser());
+        await cookieDestroyer();
+      }
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const _delete = createAsyncThunk(
+  "memories/delete",
+  async (memoryData, thunkAPI) => {
+    try {
+      const { data } = await memory._delete(memoryData);
+      const userData = await cookieExtractor(data.accessToken.data.accessToken);
+      thunkAPI.dispatch(addUser(userData));
+      return { ...data, ...memoryData };
+    } catch (error) {
+      console.log(error);
+      if (
+        error.response.data?.accessToken ||
+        error.response.data?.refreshToken
+      ) {
+        thunkAPI.dispatch(removeUser());
+        await cookieDestroyer();
+      }
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const like = createAsyncThunk(
+  "memories/like",
+  async (likeData, thunkAPI) => {
+    try {
+      const { data } = await memory.like(likeData);
+      console.log("Thunk: ", data);
+      const userData = await cookieExtractor(data.accessToken.data.accessToken);
+      thunkAPI.dispatch(addUser(userData));
+      return data;
+    } catch (error) {
+      console.log("Thunk: ", error);
+      if (
+        error.response.data?.accessToken ||
+        error.response.data?.refreshToken
+      ) {
+        thunkAPI.dispatch(removeUser());
+        await cookieDestroyer();
+      }
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
