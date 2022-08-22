eb863fd59a665ddaa379dd150bb8fa3274a00cae
Tno-MSA <tno.rappelz@gmail.com>
2022-08-22 23:43:46 +0300

    moved memoArr to separate file, addeed pagination

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,13 @@
 import { useEffect } from "react";
 import { useStyles } from "./styles";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { useLocalStorage } from "Hooks";
 //Actions
 import { getAll, like, _delete } from "store/memories/memories.thunk";
 //UI Components
-import { Common } from "components";
-import { Container, Grid } from "@mantine/core";
+import { Common, MainPage } from "components";
+import { Container } from "@mantine/core";
 
 const Home = () => {
   //Hookes
@@ -16,18 +16,21 @@ const Home = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { set } = useLocalStorage();
+  const [searchParams, setSearchParams] = useSearchParams();
   //Selectors
   const data = useSelector((state) => state.memories);
   const { user } = useSelector((state) => state.auth);
   //Checkers
   const isReady = data.memories !== null;
+  //Variables
+  const currentPage = searchParams.get("page") ? searchParams.get("page") : 1;
+
+  const handleOnPageChange = async (data) => {
+    setSearchParams({ page: data });
+  };
 
   const likeMemory = async (data) => {
-    try {
-      await dispatch(like(data));
-    } catch (error) {
-      console.log("Home: ", error);
-    }
+    await dispatch(like(data));
   };
 
   const editMemory = (data) => {
@@ -39,40 +42,32 @@ const Home = () => {
     await dispatch(_delete(data));
   };
 
-  const getAllMemories = async () => {
-    await dispatch(getAll());
+  const getAllMemories = async (page = 1) => {
+    await dispatch(getAll(page));
   };
 
   useEffect(() => {
-    getAllMemories();
-  }, []);
-
-  const MemoriesArray =
-    isReady &&
-    data.memories?.map((memory, index) => (
-      <Grid.Col
-        key={memory._id + index}
-        xs={12}
-        sm={6}
-        md={6}
-        lg={3}
-        sx={{ display: "flex", flexGrow: 1 }}
-      >
-        <Common.Cards.Memory
-          key={memory._id}
-          data={memory}
-          user={{ _id: user?._id, role: user?.role }}
-          like={likeMemory}
-          edit={editMemory}
-          _delete={deleteMemory}
-        />
-      </Grid.Col>
-    ));
+    getAllMemories(currentPage);
+  }, [currentPage]);
 
   return (
     <section className={classes.section}>
       <Container size="xl">
-        <Grid>{MemoriesArray}</Grid>
+        {!isReady && <Common.LoadingOverlay />}
+        {isReady && (
+          <MainPage.Memories
+            data={data.memories}
+            user={{ _id: user?._id, role: user?.role }}
+            like={likeMemory}
+            edit={editMemory}
+            _delete={deleteMemory}
+          />
+        )}
+        <MainPage.Pagination
+          currentPage={currentPage}
+          numberOfPages={data.numberOfPages}
+          onPageChange={handleOnPageChange}
+        />
       </Container>
     </section>
   );
