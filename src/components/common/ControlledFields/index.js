b9b49a5755ab52803657bab3c09036a12e5be9fa
Tno-MSA <tno.rappelz@gmail.com>
2022-08-22 03:57:17 +0300

    added "textarea"

--- src/components/common/ControlledFields/index.js
+++ src/components/common/ControlledFields/index.js
@@ -1,9 +1,11 @@
 import Tags from "./tags";
 import Text from "./Text";
+import Textarea from "./Textarea";
 import Password from "./Password";
 
 export default {
   Tags,
   Text,
+  Textarea,
   Password,
 };
