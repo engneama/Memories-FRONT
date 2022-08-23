f99110542f6fefc7e34022926bd133bd16d1e469
Tno-MSA <tno.rappelz@gmail.com>
2022-08-23 18:10:14 +0300

    removed css, imported useStyles

--- src/layouts/Main/Main.jsx
+++ src/layouts/Main/Main.jsx
@@ -1,18 +1,19 @@
+//Hooks
+import { useStyles } from "./styes";
 //Components
 import { Outlet } from "react-router-dom";
 //UI Components
 import Header from "layouts/Navigation/Header/Header";
 import Footer from "layouts/Navigation/Footer/Footer";
 import { FloatingButton } from "components";
-//Styles
-import "./Main.css";
 
 const Main = () => {
+  const { classes } = useStyles();
   return (
-    <div className="parent">
+    <div className={classes.parent}>
       <FloatingButton />
       <Header />
-      <div className="children">
+      <div className={classes.children}>
         <Outlet />
       </div>
       <Footer />
