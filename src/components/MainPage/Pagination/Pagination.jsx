58638ccb01cf49b5a67455591238eb5bed31a356
Tno-MSA <tno.rappelz@gmail.com>
2022-08-23 18:09:58 +0300

    removed paper

--- src/components/MainPage/Pagination/Pagination.jsx
+++ src/components/MainPage/Pagination/Pagination.jsx
@@ -1,19 +1,19 @@
 //UI Components
-import { Paper, Pagination as PaginationCom } from "@mantine/core";
+import { Pagination as PaginationCom } from "@mantine/core";
 
 const Pagination = ({ currentPage, numberOfPages, onPageChange }) => {
   return (
-    <Paper mt={60} mb={20}>
-      <PaginationCom
-        withEdges
-        withControls
-        spacing="sm"
-        position="center"
-        page={currentPage}
-        total={numberOfPages}
-        onChange={onPageChange}
-      />
-    </Paper>
+    <PaginationCom
+      mt={60}
+      mb={20}
+      withEdges
+      withControls
+      spacing="sm"
+      position="center"
+      page={currentPage}
+      total={parseInt(numberOfPages)}
+      onChange={onPageChange}
+    />
   );
 };
 
