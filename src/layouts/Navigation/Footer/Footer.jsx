5b659fae448cd82a8fd319fcf6aa58e49697e3bf
Tno-MSA <tno.rappelz@gmail.com>
2022-08-24 23:44:22 +0300

    added popover ¯\_(ツ)_/¯

--- src/layouts/Navigation/Footer/Footer.jsx
+++ src/layouts/Navigation/Footer/Footer.jsx
@@ -1,11 +1,24 @@
 //Hooks
 import { useStyles } from "./styles";
 //UI Components
-import { Container, Group, ActionIcon } from "@mantine/core";
+import { Container, Group, ActionIcon, Popover, Text } from "@mantine/core";
 import { Common } from "components";
 //Icons
 import { TbBrandInstagram } from "react-icons/tb";
 import { TbBrandTwitter, TbBrandYoutube } from "react-icons/tb";
+
+const Pop = ({ icon }) => {
+  return (
+    <Popover width={200} position="bottom" withArrow shadow="md">
+      <Popover.Target>
+        <ActionIcon size="lg">{icon}</ActionIcon>
+      </Popover.Target>
+      <Popover.Dropdown>
+        <Text>What are you waiting for? This is a fake site!</Text>
+      </Popover.Dropdown>
+    </Popover>
+  );
+};
 
 const Footer = () => {
   const { classes } = useStyles();
@@ -15,15 +28,9 @@ const Footer = () => {
       <Container className={classes.inner}>
         <Common.Logo.TextLogo />
         <Group spacing={0} className={classes.links} position="right" noWrap>
-          <ActionIcon size="lg">
-            <TbBrandTwitter size={18} />
-          </ActionIcon>
-          <ActionIcon size="lg">
-            <TbBrandYoutube size={18} />
-          </ActionIcon>
-          <ActionIcon size="lg">
-            <TbBrandInstagram size={18} />
-          </ActionIcon>
+          <Pop icon={<TbBrandTwitter size={18} />} />
+          <Pop icon={<TbBrandYoutube size={18} />} />
+          <Pop icon={<TbBrandInstagram size={18} />} />
         </Group>
       </Container>
     </div>
