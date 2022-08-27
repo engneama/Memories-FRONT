c40200f7ed859a54feddddbc9080799cd006507c
Tno-MSA <tno.rappelz@gmail.com>
2022-08-27 14:06:40 +0300

    new file

---
+++ src/components/common/Badges/Comments.jsx
@@ -0,0 +1,18 @@
+import { Badge, Text } from "@mantine/core";
+import { FaComments } from "react-icons/fa";
+
+const Comments = ({ badgeStyles, likesStyles, data }) => {
+  return (
+    <Badge
+      className={badgeStyles}
+      variant="light"
+      color="blue"
+      size="sm"
+      leftSection={<FaComments style={{ marginTop: "4px" }} />}
+    >
+      <Text className={likesStyles}>{data}</Text>
+    </Badge>
+  );
+};
+
+export default Comments;
