a83947be76e91ea52b42a00c792b4c5eaf08357c
Tno-MSA <tno.rappelz@gmail.com>
2022-08-27 14:25:40 +0300

    hide if no conent, show error msg. navigate to404

--- src/pages/user/Memories/Memories.jsx
+++ src/pages/user/Memories/Memories.jsx
@@ -2,28 +2,39 @@
 import { useEffect } from "react";
 import { useStyles } from "./styles";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import {
+  useNavigate,
+  useSearchParams,
+  useParams,
+  useLocation,
+} from "react-router-dom";
 import { useLocalStorage } from "Hooks";
 //Actions
 import { getAll, like, _delete } from "store/memories/memories.thunk";
 //UI Components
 import { Common, MainPage } from "components";
-import { Container } from "@mantine/core";
+import { Container, Title, Text, Button } from "@mantine/core";
 
 const Memories = () => {
   //Hookes
   const { classes } = useStyles();
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const { set } = useLocalStorage();
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const { username } = useParams();
   const [searchParams, setSearchParams] = useSearchParams();
   //Selectors
   const data = useSelector((state) => state.memories);
   const { user } = useSelector((state) => state.auth);
-  //Checkers
-  const isReady = data.memories !== null;
   //Variables
   const currentPage = searchParams.get("page") ? searchParams.get("page") : 1;
+  //Checkers
+  const isReady = data?.memories !== null;
+  const isExists = data?.memories?.length > 0;
+  const isMemory =
+    pathname.includes("Memories") || pathname.includes("memories");
+  const isLike = pathname.includes("Likes") || pathname.includes("likes");
 
   const handleOnPageChange = async (data) => {
     setSearchParams({ page: data });
@@ -43,7 +54,13 @@ const Memories = () => {
   };
 
   const getAllMemories = async (page = 1) => {
-    await dispatch(getAll({ page, userId: user._id }));
+    const type = isMemory ? "memories" : isLike && "likes";
+    const { payload } = await dispatch(getAll({ page, username, type }));
+    if (payload?.statusCode === 404) {
+      navigate(`/${payload?.statusCode}`, {
+        state: { code: payload.statusCode, msg: payload.message },
+      });
+    }
   };
 
   useEffect(() => {
@@ -54,20 +71,32 @@ const Memories = () => {
     <section className={classes.section}>
       <Container size="xl">
         {!isReady && <Common.LoadingOverlay />}
-        {isReady && (
-          <MainPage.Memories
-            data={data.memories}
-            user={{ _id: user?._id, role: user?.role }}
-            like={likeMemory}
-            edit={editMemory}
-            _delete={deleteMemory}
-          />
+        {isReady && !isExists && (
+          <div className={classes.notFound}>
+            <Title order={2}>Uh Oh!</Title>
+            <Text>{username} have no memories</Text>
+            <Button onClick={() => navigate(-1)} mt="md">
+              Go Back
+            </Button>
+          </div>
         )}
-        <MainPage.Pagination
-          currentPage={currentPage}
-          numberOfPages={data.numberOfPages}
-          onPageChange={handleOnPageChange}
-        />
+        {isReady && isExists && (
+          <>
+            <MainPage.Memories
+              data={data.memories}
+              user={{ _id: user?._id, role: user?.role }}
+              like={likeMemory}
+              edit={editMemory}
+              _delete={deleteMemory}
+            />
+
+            <MainPage.Pagination
+              currentPage={currentPage}
+              numberOfPages={data.numberOfPages}
+              onPageChange={handleOnPageChange}
+            />
+          </>
+        )}
       </Container>
     </section>
   );
