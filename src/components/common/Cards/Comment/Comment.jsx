4bbadb80e3da71b7cf2ffae3f9ae021b18274e9e
Tno-MSA <tno.rappelz@gmail.com>
2022-08-27 14:30:42 +0300

    added modal

--- src/components/common/Cards/Comment/Comment.jsx
+++ src/components/common/Cards/Comment/Comment.jsx
@@ -1,5 +1,6 @@
 //Hooks
 import { useStyles } from "./styles";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useLocalStorage } from "@mantine/hooks";
 //Actions
@@ -7,7 +8,7 @@ import { like, _delete } from "store/comments/comments.thunk";
 //Components
 import Moment from "react-moment";
 //UI Components
-import { Comments } from "components";
+import { Comments, Common } from "components";
 import { Paper, Text, Avatar, Group, Stack, ActionIcon } from "@mantine/core";
 //Icons
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
@@ -17,6 +18,8 @@ const Comment = ({ data, user }) => {
   const { classes } = useStyles();
   const dispatch = useDispatch();
   const [, setLocal] = useLocalStorage({ key: "editComment" });
+  //Stats
+  const [isModelOpened, setIsModelOpened] = useState(false);
   //Checkers
   const isLoggedIn = user !== null;
   let isAuthor;
@@ -34,59 +37,66 @@ const Comment = ({ data, user }) => {
   }
 
   const handleEdit = async () => {
-    console.log("Edit!");
     setLocal(data);
   };
 
   const handleDelete = async () => {
-    console.log("Delete!");
     await dispatch(_delete({ _id: data._id }));
   };
 
   const handleLike = async () => {
-    console.log("LIKE!");
     await dispatch(like({ _id: data._id, userId: user._id }));
   };
 
   return (
-    <Paper withBorder className={classes.paper}>
-      <Group position="apart">
-        <Group>
-          <Avatar
-            src={data.author.avatarURL}
-            alt={data.author.username}
-            radius="md"
-          />
-          <div>
-            <Text size="sm">{data.author.username}</Text>
-            <Text size="xs" color="dimmed">
-              <Group>
-                <Moment fromNow interval={60000}>
-                  {data.createdAt}
-                </Moment>
-              </Group>
-            </Text>
-          </div>
-        </Group>
-        {isLoggedIn && (isAuthor || isAdmin) && (
-          <Comments.OptionsButton edit={handleEdit} _delete={handleDelete} />
-        )}
-      </Group>
-
-      <Group mt="md">
-        <Stack align="center" spacing="xs">
-          {/* Like Button */}
-          {isLoggedIn && (
-            <ActionIcon onClick={handleLike}>{likeIcon}</ActionIcon>
+    <>
+      <Paper withBorder className={classes.paper}>
+        <Group position="apart">
+          <Group>
+            <Avatar
+              src={data.author.avatarURL}
+              alt={data.author.username}
+              radius="md"
+            />
+            <div>
+              <Text size="sm">{data.author.username}</Text>
+              <Text size="xs" color="dimmed">
+                <Group>
+                  <Moment fromNow interval={60000}>
+                    {data.createdAt}
+                  </Moment>
+                </Group>
+              </Text>
+            </div>
+          </Group>
+          {isLoggedIn && (isAuthor || isAdmin) && (
+            <Comments.OptionsButton
+              edit={handleEdit}
+              _delete={() => setIsModelOpened(true)}
+            />
           )}
-          {/* Number of Likes */}
-          <Text size="sm" color="dimmed">
-            {data.likes?.length} likes
-          </Text>
-        </Stack>
-        <Text className={classes.content}>{data.body}</Text>
-      </Group>
-    </Paper>
+        </Group>
+
+        <Group mt="md">
+          <Stack align="center" spacing="xs">
+            {/* Like Button */}
+            {isLoggedIn && (
+              <ActionIcon onClick={handleLike}>{likeIcon}</ActionIcon>
+            )}
+            {/* Number of Likes */}
+            <Text size="sm" color="dimmed">
+              {data.likes?.length} likes
+            </Text>
+          </Stack>
+          <Text className={classes.content}>{data.body}</Text>
+        </Group>
+      </Paper>
+      <Common.Modals.Delete
+        open={isModelOpened}
+        close={() => setIsModelOpened(false)}
+        yes={handleDelete}
+      />
+    </>
   );
 };
 
