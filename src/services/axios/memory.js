02b9ffcbc70f7f000052878a3176b39d94962271
Tno-MSA <tno.rappelz@gmail.com>
2022-08-28 18:50:35 +0300

    moved search to seperate file

--- src/services/axios/memory.js
+++ src/services/axios/memory.js
@@ -11,8 +11,6 @@ const getAll = (data) =>
   );
 const getSingle = (data) => API.get(`/memory/getSingle/${data._id}`);
 const getTags = () => API.get("/memory/getTags");
-const search = ({ query, tags }) =>
-  API.get(`/memory/search?query=${query}&tags=${tags}`);
 
 //POST
 const create = (data) => API.post("/memory/create", data);
@@ -29,7 +27,6 @@ export default {
   getAll,
   getSingle,
   getTags,
-  search,
   update,
   like,
   _delete,
