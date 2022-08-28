1367f7a13ff2b77ee2074ad9515628d12082f14b
Tno-MSA <tno.rappelz@gmail.com>
2022-08-28 18:53:31 +0300

    added search

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import Missing from "./Missing/Missing";
 import Home from "./Home/Home";
 import Memory from "./Memory";
 import User from "./user";
+import Search from "./search";
 
-export { Auth, Memory, Missing, Home, User };
+export { Auth, Memory, Missing, Home, User, Search };
