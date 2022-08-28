f0cd6b82c4baa2eb4fae60d971b4c71494e92a56
Tno-MSA <tno.rappelz@gmail.com>
2022-08-28 18:50:47 +0300

    added user and search

--- src/services/index.js
+++ src/services/index.js
@@ -2,5 +2,7 @@ import auth from "./axios/auth";
 import memory from "./axios/memory";
 import comments from "./axios/comments";
 import recommendations from "./axios/recommendations";
+import user from "./axios/user";
+import search from "./axios/search";
 
-export { auth, memory, comments, recommendations };
+export { auth, memory, comments, recommendations, user, search };
